b48661d795b926e8d9c0bc8e45ec6097beae2a47
srclauyt@gmail.com <srclauyt@gmail.com>
2022-03-28 08:47:28 -0300

    Vector3 was implemented

--- Vector3.php
+++ Vector3.php
@@ -7,4 +7,33 @@ use pocketmine\entity\Location;
 use pocketmine\world\Position;
 
 //TODO: finish this
-class Vecotr3 {}
+class Vector3 {
+  
+  public function vectorToArray(Vector3 $vector): array
+  {
+    return [
+      $vector->x,
+      $vector->y,
+      $vector->z
+    ];
+  }
+  
+  public function arrayStringToVector3(array $vector): Vector3
+  {
+    return new Vector3($vector[0], $vector[1], $vector[2]);
+  }
+  
+  /**
+   * @param [x, y, z, world, yaw, pitch]
+   */
+  public function arrayToLocation(array $vector): Location
+  {
+    return new Location($vector[0], $vector[1], $vector[2], $vector[3], $vector[4], $vector[5]);
+  }
+  
+  public function arrayToPosition(array $vector): Position
+  {
+    return new Position($vector[0], $vector[1], $vector[2], $vector[3]);
+  }
+  
+}
