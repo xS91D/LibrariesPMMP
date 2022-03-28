0798d6385b09bbeb216b4628a8ea6ed443a51052
srclauyt@gmail.com <srclauyt@gmail.com>
2022-03-28 01:05:08 -0300

    to remind

---
+++ Utils.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace libs;
+
+//TODO: finish this
+class Utils {}
---
+++ Vector3.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace libs;
+
+use pocketmine\math\Vector3;
+use pocketmine\entity\Location;
+use pocketmine\world\Position;
+
+//TODO: finish this
+class Vecotr3 {}
